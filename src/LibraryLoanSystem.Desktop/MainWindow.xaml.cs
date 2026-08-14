4b57d0c5379badcdde19c44357a2a04dccfeff97
547man <sabis5129@oslomet.no>
2026-08-15 01:07:02 +0200

    feat: add basic login window UI

--- src/LibraryLoanSystem.Desktop/MainWindow.xaml.cs
+++ src/LibraryLoanSystem.Desktop/MainWindow.xaml.cs
@@ -1,23 +1,25 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LibraryLoanSystem.Desktop;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    // Opens the link in the users default browser.
+    private void CompanyLink_RequestNavigate(
+        object sender,
+        RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true
+        });
     }
 }
